a8543ee44a41525ce1dab48d2646fae45f2e8b15
Amin <amin.ashtiani@digikala.com>
2022-12-01 16:05:28 +0330

    lesson one
    add typescript as devDependency
    add a ts file
    set languageLevel, output and entry point in ts config

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function timeout(n: number) {
+  return new Promise((res) => setTimeout(res, n))
+}
+
+export async function addNumber(a: number, b: number) {
+  await timeout(500)
+  return a + b;
+}
+
+(async () => {
+  console.log(await addNumber(3, 4))
+})()
